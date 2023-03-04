2e653cb557cde421c72db83e00869c3858488357
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-03-04 17:03:59 -0500

    Update category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,4 +43,15 @@ router.get('/:id', async (req, res) => {
 });
 
 
+// create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 
