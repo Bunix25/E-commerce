3b743fa5a0a9830c7d1b24c504caac41708d974d
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-03-04 17:06:22 -0500

    Update product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -126,21 +126,5 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', async (req, res) => {
-  // delete one product by its `id` value
-  try {
-    const productData = await Product.destroy({
-      where: {
-        id: req.params.id,
-      }
-    });
-    if (!productData){
-      return res.status(404).json({ message: 'There is no product with that id.'});
-    }
-    res.json(productData);
-  } catch (err) {
-    res.status(404).json(err);
-  }
-});
 
 module.exports = router;
