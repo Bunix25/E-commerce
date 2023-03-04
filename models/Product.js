a0399efcc798c5ef271de5a7078fdf85eb9a61bd
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-03-04 16:58:21 -0500

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -6,10 +6,10 @@ const sequelize = require('../config/connection');
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
+// set up fields and rules 
 Product.init(
   {
-    // define columns
+    // define column
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
