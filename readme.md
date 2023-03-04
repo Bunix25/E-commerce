dc4c92765e797eaf59c7cf6877d882da63e3234c
Bunix25 <svattt7@gmail.com>
2023-03-04 16:52:40 -0500

    initial commit

---
+++ readme.md
@@ -0,0 +1,60 @@
+# Object-Relational Mapping (ORM): E-Commerce Back End
+
+In this project I built out the back end for an e-commerce site by modifying starter code. Configured a working Express.js API to use Sequelize to interact with a MySQL database. This app has full CRUD capability as you can CREATE, READ, UPDATE, and DELETE from this database.
+
+
+  * [Installation](#install)
+  * [Usage](#usage)
+  * For a video on using the app click [here.](https://)
+
+
+
+## User Story
+
+- AS A manager at an internet retail company
+- I WANT a back end for my e-commerce website that uses the latest technologies
+- SO THAT my company can compete with other e-commerce companies
+
+
+
+## Install
+
+Clone project.
+Run the following line of code in your terminal to install all the needed packages: 
+```
+npm i
+```
+
+
+## Usage
+
+Once all the packages have been installed build your DataBase. You can use Mysql workbench to create your DB or run command line:
+```
+mysql -u root -p
+```
+and create your DB there. Once DB has been created run in your terminal:
+```
+npm run seed
+```
+to seed your DB. Once your DB is completed run in your terminal:
+```
+npm start
+```
+
+
+
+## Built With
+
+- JavaScript
+- Sequelize
+- Node.js
+  - dotenv
+  - express
+  - mysql2
+  - sequelize
+
+## Screen shots
+
+![Shot-1](![Alt text](images/Screenshot%201.png))
+![Shot-1](![Alt text](images/Screenshot%202.png))
+![Shot-1](![Alt text](images/Screenshot%203.png))
---
+++ routes/api/category-routes.js
@@ -0,0 +1,98 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+
+// find all categories and include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      attributes: ['id', 'category_name'],
+      include: [{
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }]
+    });
+    res.status(200).json(categoryData);
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+// find one category by its `id` value and its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }]
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+// create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+// update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      },
+    )
+    res.status(200).json(categoryData);
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+// delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
